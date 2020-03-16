d1f8f284c7de09a2a443fe4ffd3ce413c1b6ba4e
Nourez Rawji <nourez@nourez.dev>
2020-03-16 10:58:33 -0400

    added a basic contact us form

---
+++ src/pages/contact-us.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default () => (
+  <Layout>
+    <div className="sheet__inner">
+      <form name="contact" className="sheet" Netlify>
+        <h1 className="sheet__title">Let's get in Touch!</h1>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="text" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            {' '}
+            Message <input type="text" name="message" />
+          </label>
+        </p>
+        <button type="button" className="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  </Layout>
+);
